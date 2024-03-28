8fa88e47daf2cb8403611b6877d7dafddec84738
ArjunYes <codingnerd1998@gmail.com>
2024-03-28 12:47:10 +0000

    Add sign up logic

--- src/components/Signinupform.js
+++ src/components/Signinupform.js
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Signinupform = () => {
+  const [signIn, setSignIn] = useState(true);
+
   return (
     <div className="absolute inset-0 flex items-center justify-center">
       <div className="bg-black px-14 pt-14 pb-20 rounded-lg flex flex-col items-start ">
-        <h1 className="text-left mb-12 text-white text-3xl font-bold ">Sign In</h1>
+        <h1 className="text-left mb-12 text-white text-3xl font-bold ">
+           { signIn ?  'Sign In' : 'Sign Up'  }
+        </h1>
         <form action="" className="flex flex-col items-center">
           <input
             className="bg-gray-200 rounded py-3 px-4 mb-2 w-80"
             placeholder="Email or phone number"
             type="text"
           />
-          <input
-            className="bg-gray-200 rounded py-3 px-4 mb-2 w-80"
-            placeholder="Password"
-            type="text"
-          />
-          <button className="bg-red-600 text-white  py-2 px-4 mb-2 w-80">
-            Sign In
-          </button>
-        <p className="text-white mb-24">Forgot password ?</p>
+          {
+            <input
+              className="bg-gray-200 rounded py-3 px-4 mb-2 w-80"
+              placeholder="Password"
+              type="text"
+            />
+          }
+          {!signIn && (
+            <input
+              className="bg-gray-200 rounded py-3 px-4 mb-2 w-80"
+              placeholder="Re-enter Password"
+              type="text"
+            />
+          )}
+          {signIn && (
+            <button className="bg-red-600 text-white  py-2 px-4 mb-2 w-80">
+              Sign In
+            </button>
+          )}
+          {!signIn && (
+            <button className="bg-red-600 text-white  py-2 px-4 mb-2 w-80">
+              Sign Up
+            </button>
+          )}
+          <div className="mb-28">
+           { signIn && <p className="text-white">Forgot password ?</p>}
+          </div>
         </form>
         <div className="flex items-center text-white">
           <input
@@ -29,12 +51,37 @@ const Signinupform = () => {
             value="Bike"
             className="mr-2 mb-4"
           />
-          <label className="mb-4" htmlFor="vehicle1">Remember me</label>
+          <label className="mb-4" htmlFor="vehicle1">
+            Remember me
+          </label>
         </div>
-        <p className="text-gray-500">New to Netflix? <span className="text-white"> Sign up now.</span> </p>
+        {signIn && (
+          <p className="text-gray-500">
+            New to Netflix?{" "}
+            <span
+              onClick={() => setSignIn(false)}
+              className="text-white cursor-pointer hover:underline"
+            >
+              {" "}
+              Sign up now.
+            </span>{" "}
+          </p>
+        )}
+        {!signIn && (
+          <p className="text-gray-500">
+            Already have an account ?{" "}
+            <span
+              onClick={() => setSignIn(true)}
+              className="text-white cursor-pointer hover:underline"
+            >
+              {" "}
+              Sign In now.
+            </span>{" "}
+          </p>
+        )}
         <p className="text-gray-400 text-left text-xs">
-          This page is protected by Google reCAPTCHA to <br></br> ensure you're not a
-          bot. <span className="text-blue-700" > Learn more.</span>
+          This page is protected by Google reCAPTCHA to <br></br> ensure you're
+          not a bot. <span className="text-blue-700 cursor-pointer"> Learn more.</span>
         </p>
       </div>
     </div>
