e775ce42cfd2dae97e2b0a7846a7ecd85da5a18e
ArjunYes <codingnerd1998@gmail.com>
2024-04-04 01:22:51 +0100

    Sign in page form validation

--- src/components/Signinupform.js
+++ src/components/Signinupform.js
@@ -1,36 +1,61 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import { validateSignIn } from "../utils/validateSignIn";
 
 const Signinupform = () => {
   const [signIn, setSignIn] = useState(true);
+  const [errorMsg, seterrorMsg] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleSignin = () => {
+    debugger
+    console.log(email.current.value, password.current.value);
+
+    const errorMsgVal = validateSignIn(
+      email.current.value,
+      password.current.value
+    );
+    errorMsgVal ? seterrorMsg(errorMsgVal) : seterrorMsg(null);
+  };
 
   return (
     <div className="absolute inset-0 flex items-center justify-center">
       <div className="bg-black px-14 pt-14 pb-20 rounded-lg flex flex-col items-start ">
         <h1 className="text-left mb-12 text-white text-3xl font-bold ">
-           { signIn ?  'Sign In' : 'Sign Up'  }
+          {signIn ? "Sign In" : "Sign Up"}
         </h1>
-        <form action="" className="flex flex-col items-center">
+        <form
+          action=""
+          onSubmit={(e) => e.preventDefault()}
+          className="flex flex-col items-center"
+        >
           <input
+            ref={email}
             className="bg-gray-200 rounded py-3 px-4 mb-2 w-80"
             placeholder="Email or phone number"
             type="text"
           />
           {
-            <input 
+            <input
+              ref={password} 
               className="bg-gray-200 rounded py-3 px-4 mb-2 w-80"
               placeholder="Password"
-              type="text"
+              type="password"
             />
           }
           {!signIn && (
             <input
               className="bg-gray-200 rounded py-3 px-4 mb-2 w-80"
               placeholder="Re-enter Password"
-              type="text"
+              type="password"
             />
           )}
           {signIn && (
-            <button className="bg-red-600 text-white  py-2 px-4 mb-2 w-80">
+            <button
+              onClick={handleSignin}
+              className="bg-red-600 text-white  py-2 px-4 mb-2 w-80"
+            >
               Sign In
             </button>
           )}
@@ -39,50 +64,53 @@ const Signinupform = () => {
               Sign Up
             </button>
           )}
+          {errorMsg && <p className="text-red-600 py-2">{errorMsg}</p>}
           <div className="mb-28">
-           { signIn && <p className="text-white">Forgot password ?</p>}
+            {signIn && <p className="text-white">Forgot password ?</p>}
           </div>
+
+          <div className="flex items-center text-white">
+            <input
+              type="checkbox"
+              id="vehicle1"
+              name="vehicle1"
+              value="Bike"
+              className="mr-2 mb-4"
+            />
+            <label className="mb-4" htmlFor="vehicle1">
+              Remember me
+            </label>
+          </div>
+          {signIn && (
+            <p className="text-gray-500">
+              New to Netflix?{" "}
+              <span
+                onClick={() => setSignIn(false)}
+                className="text-white cursor-pointer hover:underline"
+              >
+                {" "}
+                Sign up now.
+              </span>{" "}
+            </p>
+          )}
+          {!signIn && (
+            <p className="text-gray-500">
+              Already have an account ?{" "}
+              <span
+                onClick={() => setSignIn(true)}
+                className="text-white cursor-pointer hover:underline"
+              >
+                {" "}
+                Sign In now.
+              </span>{" "}
+            </p>
+          )}
+          <p className="text-gray-400 text-left text-xs">
+            This page is protected by Google reCAPTCHA to <br></br> ensure
+            you're not a bot.{" "}
+            <span className="text-blue-700 cursor-pointer"> Learn more.</span>
+          </p>
         </form>
-        <div className="flex items-center text-white">
-          <input
-            type="checkbox"
-            id="vehicle1"
-            name="vehicle1"
-            value="Bike"
-            className="mr-2 mb-4"
-          />
-          <label className="mb-4" htmlFor="vehicle1">
-            Remember me
-          </label>
-        </div>
-        {signIn && (
-          <p className="text-gray-500">
-            New to Netflix?{" "}
-            <span
-              onClick={() => setSignIn(false)}
-              className="text-white cursor-pointer hover:underline"
-            >
-              {" "}
-              Sign up now.
-            </span>{" "}
-          </p>
-        )}
-        {!signIn && (
-          <p className="text-gray-500">
-            Already have an account ?{" "}
-            <span
-              onClick={() => setSignIn(true)}
-              className="text-white cursor-pointer hover:underline"
-            >
-              {" "}
-              Sign In now.
-            </span>{" "}
-          </p>
-        )}
-        <p className="text-gray-400 text-left text-xs">
-          This page is protected by Google reCAPTCHA to <br></br> ensure you're
-          not a bot. <span className="text-blue-700 cursor-pointer"> Learn more.</span>
-        </p>
       </div>
     </div>
   );
---
+++ src/utils/validateSignIn.js
@@ -0,0 +1,11 @@
+export const validateSignIn = (emailId, password) => {
+  const Emailregex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const passwordRegex =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+}{":;'?/>.<,])(?=.*[^\s]).{8,}$/;
+  
+
+    if( !Emailregex.test(emailId)) return "Email not valid";
+    if( !passwordRegex.test(password)) return "Pssword not valid";
+
+    return true;
+};
